0cef3264b49deb73cdef50b3b98e33f2a554d263
opriixx <iwanridwan2211@gmail.com>
2026-04-20 15:44:08 +0700

    Initial Commit

---
+++ Challenge1/Challenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge1App.swift
+//  Challenge1
+//
+//  Created by Muhammad Ridwan Novriansyah on 20/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Challenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge1
+//
+//  Created by Muhammad Ridwan Novriansyah on 20/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
